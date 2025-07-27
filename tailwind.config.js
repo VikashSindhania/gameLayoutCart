e5172c7c1f4a3ec82c9b7330dfdefbf4766f2f37
Vikash <vikashsindhania@gmail.com>
2025-07-28 00:25:12 +0530

    vercel fix update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
         primary: {
           50: "#f0f9ff",
           100: "#e0f2fe",
